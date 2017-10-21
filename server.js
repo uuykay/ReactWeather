7603062bcf64eaf9feadfe9f687e2d7898fb6ffc
Will <william.y.kuang@gmail.com>
2017-10-21 23:00:51 +1100

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express');
 
 //Create our app
 var app = express();
+// Environment variables in process.env
+const PORT = process.env.PORT || 3000;
 
+// Express middleware
+app.use(function(req, res, next){
+	if(req.headers['x-forwarded-proto'] === 'http'){
+		next();
+	}else{
+		res.redirect('http://' + req.hostname + req.url)
+	};
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-	console.log('Express server is up on port 3000');
+app.listen(PORT, function(){
+	console.log('Express server is up on PORT ' + PORT);
 });
